f1e567e248b00079dc2127bd6cc8468b0f36110b
Leslie Ledeboer <31424939+leslieledeboer@users.noreply.github.com>
2026-07-01 22:44:12 -0700

    wire SearchBar component to useGeocoding hook

--- src/hooks/useGeocoding.ts
+++ src/hooks/useGeocoding.ts
@@ -21,21 +21,28 @@ interface Failed {
 export type GeocodingStatus = Idle | Pending | Succeeded | Failed;
 
 interface ApiResult {
+  readonly id: number;
   readonly name: string;
   readonly latitude: number;
   readonly longitude: number;
   readonly population: number;
+  readonly feature_code: string;
 }
 
 interface ApiResponse {
   readonly results?: ApiResult[];
 }
 
+const normalize = (s: string) => {
+  return s.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "");
+};
+
 const mapData = (input: ApiResponse, query: string): ApiResult[] => {
   if (!input.results) return [];
 
   return input.results
-    .filter(r => r.name.startsWith(query))
+    .filter(r => r.feature_code.startsWith("PPL") || r.feature_code === "STLMT")
+    .filter(r => normalize(r.name).startsWith(normalize(query)))
     .sort((a, b) => b.population - a.population);
 };
 
