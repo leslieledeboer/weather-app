0a38b5a32ff6e4f703df9de1bd494d5636985cb3
Leslie Ledeboer <31424939+leslieledeboer@users.noreply.github.com>
2026-07-27 17:02:34 -0700

    remove debug fields and add temperature unit

--- src/components/CurrentConditions.tsx
+++ src/components/CurrentConditions.tsx
@@ -3,9 +3,7 @@ import type { CurrentWeather } from "@/hooks/useWeather.ts";
 export default function CurrentConditions({ current }: { current: CurrentWeather }) {
   return (
     <div className="p-4 rounded bg-gray-200">
-      <p>Temperature: {current.temp}</p>
-      <p>Weather Code: {current.code}</p>
-      <p>Is Day: {String(current.isDay)}</p>
+      <p className="text-2xl">{current.temp}°F</p>
       <p>Next Sunrise: {current.nextSunrise?.toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric", hour12: true })}</p>
       <p>Next Sunset: {current.nextSunset?.toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric", hour12: true })}</p>
     </div>
